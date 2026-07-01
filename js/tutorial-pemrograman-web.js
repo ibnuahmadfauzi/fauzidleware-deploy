48782654206251c1c2d2c12be9e72b6731d2f18e
ibnuahmadfauzi <ibnu7897@gmail.com>
2026-07-01 17:47:25 +0700

    new tutorial list

--- js/tutorial-pemrograman-web.js
+++ js/tutorial-pemrograman-web.js
@@ -3,4 +3,45 @@ $(document).ready(function () {
     "Pemrograman Web | Tutorial | Ibnu Ahmad Fauzi | Portofolio Web Page",
   );
   $(".tutorial-nav").addClass("active");
+
+  let htmlTutorialWeb = ``;
+
+  dataTutorialWebProgramming.forEach((item, index) => {
+    htmlTutorialWeb += `
+          <div class="col-lg-3">
+            <div class="card rounded-0 card-box-tutorial" data-id='${item.id}' style="width: 100%">
+              <img
+                src="/images/${item.logo}"
+                class="card-img-top"
+                alt="Tutorial Pemrograman Web - HTML Dasar - Ibnu Ahmad Fauzi"
+              />
+              <div class="card-body">
+                <h5 class="card-title text-center">
+                  ${item.judul} Bahasa ${item.bahasa}
+                </h5>
+              </div>
+            </div>
+          </div>
+    `;
+  });
+
+  $("#container-tutorial-web").html(htmlTutorialWeb);
+
+  $(".card-box-tutorial").on("click", function () {
+    let id = this.dataset.id;
+    dataTutorialWebProgramming.forEach((item, index) => {
+      if (item.id == id) {
+        $(".tutorial-detail-modal img").attr("src", `/images/${item.logo2}`);
+        $(".tutorial-detail-modal .detail-judul").text(item.judul);
+        $(".tutorial-detail-modal .detail-bahasa").text(item.bahasa);
+        $(".tutorial-detail-modal .detail-penyusun").text(item.penyusun);
+        $(".tutorial-detail-modal .detail-konten").text(item.jenisKonten);
+        $(".tutorial-detail-modal .detail-update").text(item.update);
+        $(".tutorial-detail-modal .detail-halaman").text(item.halaman);
+        $(".tutorial-detail-modal .detail-deskripsi").text(item.deskripsi);
+        $(".detail-link").attr("href", item.link);
+      }
+    });
+    $("#modalDetailTutorial").modal("show");
+  });
 });
---
+++ utils/data-tutorial.js
@@ -0,0 +1,30 @@
+let dataTutorialWebProgramming = [
+  {
+    id: 1,
+    logo: "sampul-html-dasar.png",
+    logo2: "logo-html.png",
+    judul: "Tutorial HTML Dasar",
+    bahasa: "Indonesia",
+    penyusun: "Ibnu Ahmad Fauzi",
+    jenisKonten: "Teks Bacaan",
+    update: "01 Juli 2026",
+    halaman: "12",
+    deskripsi:
+      "Materi tutorial HTML Dasar ini dirancang untuk membantu pembaca menguasai bahasa markup HTML secara menyeluruh tanpa melibatkan CSS maupun JavaScript. Pembelajaran dimulai dari persiapan lingkungan pengembangan, pengenalan struktur dokumen HTML, pembuatan berbagai jenis konten seperti teks, gambar, tabel, dan formulir, hingga penerapan Semantic HTML, SEO dasar, aksesibilitas, serta pengembangan website multi-halaman. Setiap materi disusun secara sistematis dan dilengkapi praktik langsung agar pembaca tidak hanya memahami teori, tetapi juga mampu membangun struktur website yang baik sesuai standar pengembangan web modern. Setelah menyelesaikan seluruh materi, pembaca akan memiliki fondasi HTML yang kuat dan siap melanjutkan ke tahap pembelajaran CSS untuk mempercantik tampilan website.",
+    link: "/html-dasar-bahasa-indonesia/pengenalan-html.html",
+  },
+  {
+    id: 2,
+    logo: "sampul-php-dasar.png",
+    logo2: "logo-php.png",
+    judul: "Tutorial PHP Dasar",
+    bahasa: "Indonesia",
+    penyusun: "Ibnu Ahmad Fauzi",
+    jenisKonten: "Teks Bacaan",
+    update: "01 Juli 2026",
+    halaman: "-",
+    deskripsi:
+      "Dasar-Dasar PHP merupakan tahap awal dalam mempelajari bahasa pemrograman PHP yang berfokus pada pemahaman konsep-konsep fundamental sebagai landasan dalam pengembangan aplikasi web. Pada materi ini, pembaca akan mempelajari cara kerja PHP sebagai bahasa pemrograman sisi server (server-side scripting), sintaks dasar, penggunaan variabel dan tipe data, operator, percabangan, perulangan, fungsi, array, hingga pengolahan data dari formulir HTML. Selain itu, dibahas pula penggunaan superglobal, pengelolaan sesi (session), cookie, upload file, pemisahan kode menggunakan include dan require, serta penanganan kesalahan (error handling). Melalui kombinasi penjelasan teori, contoh kode, dan latihan praktik, pembaca akan memiliki kemampuan untuk membangun aplikasi web dinamis sederhana menggunakan PHP sebagai bekal sebelum mempelajari konsep-konsep yang lebih lanjut seperti pemrograman berorientasi objek (OOP), database, dan framework PHP.",
+    link: "#",
+  },
+];
